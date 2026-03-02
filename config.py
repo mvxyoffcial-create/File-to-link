e2038947195bc97fce87faae5284f23e88307f08
mvxyoffcial-create <mvxyoffcial@gmail.com>
2026-03-02 13:12:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,26 +5,26 @@ load_dotenv()
 
 class Config:
     # Bot Config
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", 0))
-    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = "8731151889:AAGHWDNuYdAOOj2PyUwtyXVt8loyzCkwsCU"
+    API_ID = 20288994
+    API_HASH = "d702614912f1ad370a0d18786002adbf"
     
     # Owner & Admins
-    OWNER_ID = int(os.environ.get("OWNER_ID", 0))
+    OWNER_ID = 8498741978
     ADMINS = [int(x) for x in os.environ.get("ADMINS", "").split() if x]
     
     # Channels
-    BIN_CHANNEL = int(os.environ.get("BIN_CHANNEL", 0))  # Private storage channel
-    FORCE_SUB_CHANNEL_1 = os.environ.get("FORCE_SUB_CHANNEL_1", "zerodev2")
-    FORCE_SUB_CHANNEL_2 = os.environ.get("FORCE_SUB_CHANNEL_2", "mvxyoffcail")
+    BIN_CHANNEL = -1003853860662  # Private storage channel
+    FORCE_SUB_CHANNEL_1 = "zerodev2"
+    FORCE_SUB_CHANNEL_2 = "mvxyoffcail"
     
     # MongoDB
-    MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017")
-    DATABASE_NAME = os.environ.get("DATABASE_NAME", "filestreambot")
+    MONGO_URI = "mongodb+srv://Zerobothost:zero8907@cluster0.szwdcyb.mongodb.net/?appName=Cluster07"
+    DATABASE_NAME = "filestreambot"
     
     # Web Server
     BASE_URL = os.environ.get("BASE_URL", "http://localhost:8080")
-    PORT = int(os.environ.get("PORT", 8080))
+    PORT = 8080
     HOST = os.environ.get("HOST", "0.0.0.0")
     
     # Workers
